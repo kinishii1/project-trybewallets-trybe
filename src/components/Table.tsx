cdf6034d7aa040db6b3437a4ef0460082759dc34
Kin Ishii <kinishii97@gmail.com>
2023-11-15 12:50:04 +0900

    implementando a table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,47 @@
+import { useSelector } from "react-redux";
+
 function Table() {
-  return <div>Table</div>;
+  const { expenses } = useSelector((state) => state.wallet);
+  return (
+    <table>
+      <tr>
+        <th>Descrição</th>
+        <th>Tag</th>
+        <th>Método de pagamento</th>
+        <th>Valor</th>
+        <th>Moeda</th>
+        <th>Câmbio utilizado</th>
+        <th>Valor convertido</th>
+        <th>Moeda de conversão</th>
+        <th>Editar/Excluir</th>
+      </tr>
+      {expenses.map((expense) => (
+        <tr key={expense.id}>
+          <td>{expense.description}</td>
+          <td>{expense.tag}</td>
+          <td>{expense.method}</td>
+          <td>
+            {Number(expense.value).toLocaleString("pt-br", {
+              style: "currency",
+              currency: 'BRL',
+            })}
+          </td>
+          <td>{expense.exchangeRates[expense.currency].name}</td>
+          <td>{expense.exchangeRates[expense.currency].ask}</td>
+          <td>
+            {(
+              expense.value * expense.exchangeRates[expense.currency].ask
+            ).toFixed(2)}
+          </td>
+          <td>Real</td>
+          <td>
+            <button>Editar</button>
+            <button>Excluir</button>
+          </td>
+        </tr>
+      ))}
+    </table>
+  );
 }
 
 export default Table;
--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
+import Table from "../components/Table";
 import WalletForm from "../components/WalletForm";
 
 function Wallet() {
   return (
     <>
-      <Header />;
-      <WalletForm />;
+      <Header />
+      <WalletForm />
+      <Table />
     </>
   );
 }
